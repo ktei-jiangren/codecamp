c80c95e80282f2e7c222da690bbf03f71581ec84
Rui Cheng <ktei2008@gmail.com>
2018-08-03 21:41:36 +1000

    erroroverlay

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
-
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
 });
 
+const errorOverlayPlugin = new ErrorOverlayPlugin();
+
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
   module: {
     rules: [
+      {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        use: ["eslint-loader"],
+        exclude: /node_modules/
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -23,5 +35,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlPlugin, errorOverlayPlugin]
 };
