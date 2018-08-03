dd3a14e6689849f6c6ce675e32394c7d7013074a
Rui Cheng <ktei2008@gmail.com>
2018-08-03 21:15:08 +1000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
